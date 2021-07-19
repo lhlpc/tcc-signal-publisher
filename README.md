e8155228f626e645c5adb1ee185bee683d948eec
Henrique <henrique@Henriques-MacBook-Pro.local>
2021-07-19 15:22:25 -0300

    refactor: remove unnecessary comments and add README file

---
+++ README.md
@@ -0,0 +1,9 @@
+# Software Captador de sinais do TCC
+
+[Link para TCC escrito](https://flutter.dev/docs/get-started/install)
+
+## Execução
+
+- Executar numa placa que tenha o SoC ESP32
+- Usar bibliotecas e IDE do Arduino para executar o projeto
+- Descomentar código que define SSID e PASSWORD do WiFi da rede utilizada (linhas 6 e 7)
--- signal_publisher/signal_publisher.ino
+++ signal_publisher/signal_publisher.ino
@@ -1,49 +1,45 @@
-// https://www.filipeflop.com/blog/esp32-e-mqtt-dashboard-android/
 #include <WiFi.h>
 #include <PubSubClient.h>
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
 
+//const char* SSID = ""; // SSID / nome da rede WI-FI que deseja se conectar
+//const char* PASSWORD = ""; // Senha da rede WI-FI que deseja se conectar
+
 
 // JSON to send
-//const int capacity = JSON_OBJECT_SIZE(1) + JSON_ARRAY_SIZE(2);
 const int capacity = JSON_ARRAY_SIZE(2) + 4 * JSON_OBJECT_SIZE(2);
 StaticJsonDocument<capacity> doc;
 char jsonBuffer[256];
 JsonArray signalSamples = doc.createNestedArray("signalSamples");
 
- 
+
 /* Definições do LED */
 #define PIN_LED     25
- 
+
 /* Defines do MQTT */
 #define EMG_PUBLISH_TOPIC "lpctcc/emg"
- 
+
 #define MQTT_CLIENT_ID  "clientId-pvAkhZbJdW"     //id mqtt (para identificação de sessão)
-                                  //IMPORTANTE: este deve ser único no broker (ou seja, 
-                                  //            se um client MQTT tentar entrar com o mesmo 
-                                  //            id de outro já conectado ao broker, o broker 
-                                  //            irá fechar a conexão de um deles).
+//IMPORTANTE: este deve ser único no broker (ou seja,
+//            se um client MQTT tentar entrar com o mesmo
+//            id de outro já conectado ao broker, o broker
+//            irá fechar a conexão de um deles).
 #define MQTT_USERNAME "useraskjas"
 #define MQTT_PASSWORD "ajshdajsdhv"
 
-//#define EMG_PIN 32 // EMG mesmo
-#define EMG_PIN 33 // Resistencia variavel
- 
+#define EMG_PIN 35 // EMG mesmo
+
 /* Variaveis, constantes e objetos globais */
 //DHT dht(DHTPIN, DHTTYPE);
- 
-const char* SSID = "LHL"; // SSID / nome da rede WI-FI que deseja se conectar
-const char* PASSWORD = "henrique94"; // Senha da rede WI-FI que deseja se conectar
- 
+
 const char* BROKER_MQTT = "broker.hivemq.com"; //URL do broker MQTT que se deseja utilizar
 int BROKER_PORT = 1883; // Porta do Broker MQTT
-   
+
 //Variáveis e objetos globais
 WiFiClient espClient; // Cria o objeto espClient
 PubSubClient MQTT(espClient); // Instancia o Cliente MQTT passando o objeto espClient
- 
-/* Prototypes */
+
 
 void initWiFi(void);
 void initMQTT(void);
@@ -51,195 +47,119 @@ void mqtt_callback(char* topic, byte* payload, unsigned int length);
 void reconnectMQTT(void);
 void reconnectWiFi(void);
 void VerificaConexoesWiFIEMQTT(void);
- 
-/*
- * Implementações
- */
- 
-/* Função: inicializa e conecta-se na rede WI-FI desejada
-*  Parâmetros: nenhum
-*  Retorno: nenhum
-*/
-void initWiFi(void) 
+
+void initWiFi(void)
 {
-    delay(10);
-    Serial.println("------Conexao WI-FI------");
-    Serial.print("Conectando-se na rede: ");
-    Serial.println(SSID);
-    Serial.println("Aguarde");
-      
-    reconnectWiFi();
+  delay(10);
+  Serial.println("------Conexao WI-FI------");
+  Serial.print("Conectando-se na rede: ");
+  Serial.println(SSID);
+  Serial.println("Aguarde");
+
+  reconnectWiFi();
 }
- 
-/* Função: inicializa parâmetros de conexão MQTT(endereço do 
- *         broker, porta e seta função de callback)
- * Parâmetros: nenhum
- * Retorno: nenhum
- */
-void initMQTT(void) 
+
+
+void initMQTT(void)
 {
-    MQTT.setServer(BROKER_MQTT, BROKER_PORT);   //informa qual broker e porta deve ser conectado
-//    MQTT.setCallback(mqtt_callback);         //atribui função de callback (função chamada quando qualquer informação de um dos tópicos subescritos chega)
+  MQTT.setServer(BROKER_MQTT, BROKER_PORT);   //informa qual broker e porta deve ser conectado
 }
- 
-/* Função: função de callback 
- *         esta função é chamada toda vez que uma informação de 
- *         um dos tópicos subescritos chega)
- * Parâmetros: nenhum
- * Retorno: nenhum
- */
-//void mqtt_callback(char* topic, byte* payload, unsigned int length) 
-//{
-//    String msg;
-//  
-//    /* obtem a string do payload recebido */
-//    for(int i = 0; i < length; i++) 
-//    {
-//       char c = (char)payload[i];
-//       msg += c;
-//    }
-// 
-//    Serial.print("Chegou a seguinte string via MQTT: ");
-//    Serial.println(msg);
-//    
-//    /* toma ação dependendo da string recebida */
-//    if (msg.equals("1"))
-//    {
-////        digitalWrite(PIN_LED, HIGH);
-//        Serial.print("LED aceso mediante comando MQTT");
-//    }
-//  
-//    if (msg.equals("0"))
-//    {
-////        digitalWrite(PIN_LED, LOW);    
-//        Serial.print("LED apagado mediante comando MQTT"); 
-//    }
-//}
- 
-/* Função: reconecta-se ao broker MQTT (caso ainda não esteja conectado ou em caso de a conexão cair)
- *         em caso de sucesso na conexão ou reconexão, o subscribe dos tópicos é refeito.
- * Parâmetros: nenhum
- * Retorno: nenhum
- */
-void reconnectMQTT(void) 
+
+void reconnectMQTT(void)
 {
-    while (!MQTT.connected()) 
+  while (!MQTT.connected())
+  {
+    Serial.print("* Tentando se conectar ao Broker MQTT: ");
+    Serial.println(BROKER_MQTT);
+    Serial.println(MQTT.state());
+    Serial.println(MQTT_CLIENT_ID);
+    Serial.println(MQTT_USERNAME);
+    Serial.println(MQTT_PASSWORD);
+    Serial.println(BROKER_MQTT);
+    Serial.println(BROKER_PORT);
+    if (MQTT.connect(MQTT_CLIENT_ID))
     {
-        Serial.print("* Tentando se conectar ao Broker MQTT: ");
-        Serial.println(BROKER_MQTT);
-        Serial.println(MQTT.state());
-        Serial.println(MQTT_CLIENT_ID);
-        Serial.println(MQTT_USERNAME);
-        Serial.println(MQTT_PASSWORD);
-        Serial.println(BROKER_MQTT);
-        Serial.println(BROKER_PORT);
-        if (MQTT.connect(MQTT_CLIENT_ID)) 
-        {
-            Serial.println("Conectado com sucesso ao broker MQTT!");
-//            MQTT.subscribe(TOPICO_SUBSCRIBE_LED); 
-        } 
-        else
-        {
-            Serial.println("Falha ao reconectar no broker.");
-            Serial.println("Havera nova tentatica de conexao em 2s");
-            delay(2000);
-        }
+      Serial.println("Conectado com sucesso ao broker MQTT!");
     }
+    else
+    {
+      Serial.println("Falha ao reconectar no broker.");
+      Serial.println("Havera nova tentatica de conexao em 2s");
+      delay(2000);
+    }
+  }
 }
- 
-/* Função: verifica o estado das conexões WiFI e ao broker MQTT. 
- *         Em caso de desconexão (qualquer uma das duas), a conexão
- *         é refeita.
- * Parâmetros: nenhum
- * Retorno: nenhum
- */
+
+/* Função: verifica o estado das conexões WiFI e ao broker MQTT.
+           Em caso de desconexão (qualquer uma das duas), a conexão
+           é refeita.
+   Parâmetros: nenhum
+   Retorno: nenhum
+*/
 void VerificaConexoesWiFIEMQTT(void)
 {
-    if (!MQTT.connected()){
-      reconnectMQTT(); //se não há conexão com o Broker, a conexão é refeita
-    }
-        
-     reconnectWiFi(); //se não há conexão com o WiFI, a conexão é refeita
+  if (!MQTT.connected()) {
+    reconnectMQTT(); //se não há conexão com o Broker, a conexão é refeita
+  }
+
+  reconnectWiFi(); //se não há conexão com o WiFI, a conexão é refeita
 }
- 
+
 /* Função: reconecta-se ao WiFi
- * Parâmetros: nenhum
- * Retorno: nenhum
- */
-void reconnectWiFi(void) 
+   Parâmetros: nenhum
+   Retorno: nenhum
+*/
+void reconnectWiFi(void)
 {
-    //se já está conectado a rede WI-FI, nada é feito. 
-    //Caso contrário, são efetuadas tentativas de conexão
-    if (WiFi.status() == WL_CONNECTED)
-        return;
-          
-    WiFi.begin(SSID, PASSWORD); // Conecta na rede WI-FI
-      
-    while (WiFi.status() != WL_CONNECTED) 
-    {
-        delay(100);
-        Serial.print(".");
-    }
-    
-    Serial.println();
-    Serial.print("Conectado com sucesso na rede ");
-    Serial.print(SSID);
-    Serial.println("IP obtido: ");
-    Serial.println(WiFi.localIP());
+  //se já está conectado a rede WI-FI, nada é feito.
+  //Caso contrário, são efetuadas tentativas de conexão
+  if (WiFi.status() == WL_CONNECTED)
+    return;
+
+  WiFi.begin(SSID, PASSWORD); // Conecta na rede WI-FI
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(100);
+    Serial.print(".");
+  }
+
+  Serial.println();
+  Serial.print("Conectado com sucesso na rede ");
+  Serial.print(SSID);
+  Serial.println("IP obtido: ");
+  Serial.println(WiFi.localIP());
 }
-  
-/* Função de setup */
-void setup() 
+
+/* Função de setup inicial do software */
+void setup()
 {
-    Serial.begin(115200);  
+  Serial.begin(115200);
 
-    Serial.print("++++++ alou 1");
+  doc["idUser"] = 10;
 
-//    doc["idDevice"] = 456;
-    doc["idUser"] = 1;
-//    JsonArray signalSamples = doc.createNestedArray("signalSamples");
-//    signalSamples[0] = 41.2;
-//    signalSamples[1] = 53.9;
-//    serializeJson(doc, jsonBuffer);
- 
-    /* Inicializa a conexao wi-fi */
-    initWiFi();
- 
-    /* Inicializa a conexao ao broker MQTT */
-    initMQTT();
+  /* Inicializa a conexao WiFi */
+  initWiFi();
+
+  /* Inicializa a conexao ao broker MQTT */
+  initMQTT();
 }
- 
+
 /* Loop principal */
-void loop() 
+void loop()
 {
 
-  Serial.println("++++++ alou");
   signalSamples[0] = analogRead(EMG_PIN);
   serializeJson(doc, jsonBuffer);
 
   Serial.println(jsonBuffer);
-  
 
-
-     
-    /* garante funcionamento das conexões WiFi e ao broker MQTT */
   VerificaConexoesWiFIEMQTT();
 
-//  HTTPClient httpClient;
-//  httpClient.begin("https://jsonplaceholder.typicode.com/todos/1");
-//  int httpCode = httpClient.GET();
-//  if(httpCode > 0) {
-//    String payload = httpClient.getString();  
-//    Serial.println(payload);
-//  } else {
-//    Serial.println("Error on HTTP");
-//   }
+  // Envia sinal captado para conexão com broket MQTT
+  MQTT.publish(EMG_PUBLISH_TOPIC, jsonBuffer);
 
-  Serial.println("++++++ alou 3");
-//    MQTT.publish(EMG_PUBLISH_TOPIC, "teste 3");
-    MQTT.publish(EMG_PUBLISH_TOPIC, jsonBuffer);
-    MQTT.loop(); // keep-alive da comunicação com broker MQTT
-
-//    /* Refaz o ciclo após 2 segundos */
-    delay(300);
+  // Manter conexão com broker MQTT ativa
+  MQTT.loop();
+  delay(300);
 }
